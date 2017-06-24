17f68633e9ae03d3dfa17182f63a72c59a6743a1
Mattia Ricci <mattia.ricci8@studio.unibo.it>
2017-06-24 10:44:23 +0200

    MVC architecture

---
+++ src/main/java/view/BasePanel.java
@@ -0,0 +1,45 @@
+package view;
+
+import utilities.Settings;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+class BasePanel extends ImagePanel {
+
+    private static final long serialVersionUID = 1L;
+    private static final int INSETS = 10;
+    protected JButton backButton;
+    protected JPanel centralPanel;
+    protected GridBagConstraints k;
+
+    public BasePanel() {
+
+        this.setLayout(new BorderLayout());
+        this.centralPanel = new JPanel(new GridBagLayout());
+        final JPanel downPanel = new JPanel(new BorderLayout());
+
+        final ImageIcon icon = new ImageIcon(BasePanel.class.getResource(Settings.IMAGES_FOLDER()
+                + "arrow-back.png"));
+        this.backButton = new JButton(icon);
+        this.backButton.setContentAreaFilled(false);
+        this.backButton.setBorder(null);
+        downPanel.add(this.backButton, BorderLayout.WEST);
+        downPanel.setOpaque(false);
+        this.add(downPanel, BorderLayout.SOUTH);
+
+        this.k = new GridBagConstraints();
+        this.k.gridy = 0;
+        this.k.insets = new Insets(INSETS, INSETS, INSETS, INSETS);
+        this.k.fill = GridBagConstraints.VERTICAL;
+        this.centralPanel.setOpaque(false);
+        this.add(centralPanel, BorderLayout.CENTER);
+    }
+
+
+}
---
+++ src/main/java/view/View.java
@@ -0,0 +1,24 @@
+package view;
+
+import controller.Controller;
+import model.game.Model;
+
+public interface View {
+
+    void setController(Controller controller);
+
+    void drawModel(Model model);
+
+    void showMenu();
+
+    void showLogin();
+
+    void showSignIn();
+
+    void showGame();
+
+    void showPause();
+
+    void showError(final String error, final String title);
+
+}
