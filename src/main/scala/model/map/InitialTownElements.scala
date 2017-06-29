866209c91cd1426adcb4f8324194bac81d2e5f62
Luca Polverelli <luca.polverelli3@studio.unibo.it>
2017-06-29 12:56:44 +0200

    Fixed map center

--- src/main/scala/model/map/InitialTownElements.scala
+++ src/main/scala/model/map/InitialTownElements.scala
@@ -4,13 +4,15 @@ import model.environment.CoordinateImpl
 import utilities.Settings
 
 case class InitialTownElements() extends MapElementsImpl {
-
   for (x <- 0 until Settings.MAP_WIDTH)
     for (y <- 0 until Settings.MAP_HEIGHT)
       if (x == 0 || x == Settings.MAP_WIDTH - 1 || y == 0 || y == Settings.MAP_HEIGHT - 1) addTile(Tree(), CoordinateImpl(x,y))
 
-  addTile(PokemonCenter(CoordinateImpl(15,30)), CoordinateImpl(15,30))
-  addTile(Laboratory(CoordinateImpl(15,15)), CoordinateImpl(15,15))
+  val coordinatePokemonCenter = CoordinateImpl(15,30)
+  val coordinateLaboratory = CoordinateImpl(15,15)
+
+  addTile(PokemonCenter(coordinatePokemonCenter), coordinatePokemonCenter)
+  addTile(Laboratory(coordinateLaboratory), coordinateLaboratory)
 
   for (x <- 30 until 40)
     for (y <- 30 until 40)
@@ -19,5 +21,4 @@ case class InitialTownElements() extends MapElementsImpl {
   for (x <- 20 until 25)
     for (y <- 20 until 25)
       addTile(TallGrass(), CoordinateImpl(x,y))
-
 }
