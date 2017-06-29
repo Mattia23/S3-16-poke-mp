7ee73107dacf313e6ed48fd11710c977d6cd39a1
Giulia Lucchi <giulia.lucchi3@studio.unibo.it>
2017-06-29 15:29:42 +0200

    Added elements in InitialTownElements.scala

--- src/main/scala/model/map/InitialTownElements.scala
+++ src/main/scala/model/map/InitialTownElements.scala
@@ -4,55 +4,12 @@ import model.environment.CoordinateImpl
 import utilities.Settings
 
 case class InitialTownElements() extends MapElementsImpl {
-  for (x <- 0 until Settings.MAP_WIDTH)
-    for (y <- 0 until Settings.MAP_HEIGHT)
-      if (x == 0 || x == Settings.MAP_WIDTH - 1 || y == 0 || y == Settings.MAP_HEIGHT - 1) addTile(Tree(), CoordinateImpl(x,y))
 
-  addTile(PokemonCenter(CoordinateImpl(10,19)), CoordinateImpl(10,19))
-  addTile(Laboratory(CoordinateImpl(40,20)), CoordinateImpl(40,20))
+  addBuildings()
+  addLakes()
+  addTallGrass()
+  addTrees()
 
-  addLake(CoordinateImpl(4,5), CoordinateImpl(20,10))
-  addLake(CoordinateImpl(40,40), CoordinateImpl(48,48))
-  addLake(CoordinateImpl(6,28), CoordinateImpl(15,30))
-
-  for (x <- 6 to 15)
-    addTile(TallGrass(), CoordinateImpl(x,31))
-  for (y <- 28 to 31) {
-    addTile(TallGrass(), CoordinateImpl(5, y))
-    addTile(TallGrass(), CoordinateImpl(16, y))
-  }
-
-  for (x <- 1 to 20)
-    for (y <- 1 to 4)
-      addTile(TallGrass(), CoordinateImpl(x,y))
-
-  for (x <- 1 to 3)
-    for (y <- 5 to 10)
-      addTile(TallGrass(), CoordinateImpl(x,y))
-
-  for (x <- 1 to 9)
-    for (y <- 19 to 23)
-      addTile(TallGrass(), CoordinateImpl(x,y))
-
-  for (x <- 39 to 48)
-    for (y <- 1 to 10)
-      addTile(TallGrass(), CoordinateImpl(x,y))
-
-  for (y <- 1 to 8)
-    addTile(TallGrass(), CoordinateImpl(38,y))
-
-  for (x <- 36 to 37)
-    for (y <- 1 to 5)
-      addTile(TallGrass(), CoordinateImpl(x,y))
-
-
-  for (x <- 1 to 14)
-      addTile(Tree(), CoordinateImpl(x,18))
-
-  for (x <- 43 to 49)
-    for (y <- 1 to 4)
-      addTile(Tree(), CoordinateImpl(x,y))
-  
   mainRoad()
   square()
 
@@ -92,8 +49,77 @@ case class InitialTownElements() extends MapElementsImpl {
         }
   }
 
+  private def addBuildings(): Unit ={
+    addTile(PokemonCenter(CoordinateImpl(10,19)), CoordinateImpl(10,19))
+    addTile(Laboratory(CoordinateImpl(40,20)), CoordinateImpl(40,20))
+  }
 
+  private def addTrees(): Unit ={
+    for (x <- 0 until Settings.MAP_WIDTH)
+      for (y <- 0 until Settings.MAP_HEIGHT)
+        if (x == 0 || x == Settings.MAP_WIDTH - 1 || y == 0 || y == Settings.MAP_HEIGHT - 1) addTile(Tree(), CoordinateImpl(x,y))
 
+    for (x <- 1 to 14)
+      addTile(Tree(), CoordinateImpl(x,18))
 
+    for (x <- 15 to 16)
+      for(y <- 18 to 23)
+      addTile(Tree(), CoordinateImpl(x,y))
+
+    for (x <- 43 to 49)
+      for (y <- 1 to 4)
+        addTile(Tree(), CoordinateImpl(x,y))
+
+    val trees = Seq[CoordinateImpl](CoordinateImpl(39,23), CoordinateImpl(47,23))
+    addTileInMultipleCoordinates(Tree(), trees)
+  }
+
+  private def addLakes(): Unit ={
+    addLake(CoordinateImpl(4,5), CoordinateImpl(20,10))
+    addLake(CoordinateImpl(40,40), CoordinateImpl(48,48))
+    addLake(CoordinateImpl(6,28), CoordinateImpl(15,30))
+    addLake(CoordinateImpl(10,42), CoordinateImpl(11,44))
+  }
+
+  private def addTallGrass(): Unit ={
+    for (x <- 6 to 15)
+      addTile(TallGrass(), CoordinateImpl(x,31))
+
+    for (y <- 28 to 31) {
+      addTile(TallGrass(), CoordinateImpl(5,y))
+      addTile(TallGrass(), CoordinateImpl(16,y))
+    }
+
+    for (x <- 1 to 20)
+      for (y <- 1 to 4)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (x <- 1 to 3)
+      for (y <- 5 to 10)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (x <- 1 to 9)
+      for (y <- 19 to 23)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (x <- 1 to 8)
+      for (y <- 24 to 27)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (x <- 39 to 48)
+      for (y <- 1 to 10)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (y <- 1 to 8)
+      addTile(TallGrass(), CoordinateImpl(38,y))
+
+    for (x <- 36 to 37)
+      for (y <- 1 to 5)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+
+    for (x <- 42 to 48)
+      for (y <- 11 to 13)
+        addTile(TallGrass(), CoordinateImpl(x,y))
+  }
 
 }
