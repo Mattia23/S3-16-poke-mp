545c841b95fb24b042b4c8423b14b8edcb77606e
Francesco Dicara <dicaraf@gmail.com>
2017-07-03 16:21:46 +0200

    demo

--- src/main/scala/app/CreazionePokemon.scala
+++ src/main/scala/app/CreazionePokemon.scala
@@ -11,16 +11,18 @@ import utilities.Settings
 import view.BattlePanel
 
 object CreazionePokemon extends App {
+  val row: Int = Settings.FRAME_WIDTH/100
   val frame: JFrame = new JFrame()
   val mainPanel: BattlePanel = new BattlePanel()
   var imageIcon: String = new String
   var imageIcon2: String = new String
   frame.setSize(Settings.FRAME_WIDTH, Settings.FRAME_WIDTH)
+  println(frame.getSize)
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-  mainPanel.setLayout(new GridLayout(7,6))
-  val panelHolder = Array.ofDim[JPanel](7, 6)
-  for(i <- 0 until 7) {
-    for(j <- 0 until 6) {
+  mainPanel.setLayout(new GridLayout(row,row))
+  val panelHolder = Array.ofDim[JPanel](row, row)
+  for(i <- 0 until row) {
+    for(j <- 0 until row) {
       panelHolder(i)(j) = new JPanel()
       panelHolder(i)(j).setOpaque(false)
       mainPanel.add(panelHolder(i)(j))
@@ -33,25 +35,27 @@ object CreazionePokemon extends App {
   val buttonPoke2: JButton = new JButton()
   this.buttonPoke2.setContentAreaFilled(false)
   this.buttonPoke2.setBorder(null)
+  this.buttonPoke2.set
   val textField: JTextField = new JTextField("Pokemon")
   val button: JButton = new JButton("Crea pokemon")
 
   button.addActionListener((e) => {
+    val size: Int = Settings.FRAME_WIDTH/row
     val p1: PokemonWithLife = PokemonFactory.createPokemon(Owner.WILD,Optional.empty(),Optional.of(15)).get()
     val p2: PokemonWithLife = PokemonFactory.createPokemon(Owner.WILD,Optional.empty(),Optional.of(15)).get()
     textField.setText(p1.pokemon.name)
     imageIcon = Settings.POKEMON_IMAGES_FRONT_FOLDER + p1.pokemon.id + ".png"
     var myImage = ImageIO.read(getClass.getResource(imageIcon))
-    val myImageIcon = new ImageIcon(myImage)
+    val myImageIcon = new ImageIcon(myImage.getScaledInstance(size, size, java.awt.Image.SCALE_SMOOTH))
     buttonPoke.setIcon(myImageIcon)
     imageIcon2 = Settings.POKEMON_IMAGES_BACK_FOLDER + p2.pokemon.id + ".png"
     var myImage2 = ImageIO.read(getClass.getResource(imageIcon2))
-    val myImageIcon2 = new ImageIcon(myImage2)
+    val myImageIcon2 = new ImageIcon(myImage2.getScaledInstance(size, size, java.awt.Image.SCALE_SMOOTH))
     buttonPoke2.setIcon(myImageIcon2)
   })
-  panelHolder(1)(4).add(buttonPoke)
-  panelHolder(4)(1).add(buttonPoke2)
-  panelHolder(5)(3).add(button)
+  panelHolder(1)(3).add(buttonPoke)
+  panelHolder(2)(0).add(buttonPoke2)
+  panelHolder(2)(2).add(button)
   frame.add(mainPanel)
   frame.setVisible(true)
 }
