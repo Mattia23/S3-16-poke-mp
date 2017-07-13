19b43ec5b2277e5cf13cffc1acf51469ab027ab5
Giulia Lucchi <giulia.lucchi3@studio.unibo.it>
2017-07-13 18:02:17 +0200

    Improved code in MapController

--- src/main/scala/controller/MapController.scala
+++ src/main/scala/controller/MapController.scala
@@ -13,6 +13,8 @@ import scala.util.Random
 class MapController(private val view: View, private val _trainer: Trainer) extends GameControllerImpl(view, _trainer){
   private final val RANDOM_MAX_VALUE = 10
   private final val MIN_VALUE_TO_FIND_POKEMON = 8
+  private final val POKEMON_CENTER_BUILDING = "Pokemon center"
+  private final val LABORATORY_BUILDING = "Laboratory"
 
   private val gameMap = MapCreator.create(Settings.MAP_HEIGHT, Settings.MAP_WIDTH, InitialTownElements())
   private var lastCoordinates: Coordinate = _
@@ -29,16 +31,22 @@ class MapController(private val view: View, private val _trainer: Trainer) exten
 
   private def doFirstLogin(): Unit = {
     pause()
+    updateLastCoordinateToBuilding(LABORATORY_BUILDING)
+    new LaboratoryController(this.view, this, trainer).start()
+  }
+
+  private def updateLastCoordinateToBuilding(building: String): Unit = {
     for( x <- 0 until gameMap.width){
       for( y <- 0 until gameMap.height){
-        gameMap.map(x)(y) match {
-          case tile:Laboratory =>
+        (building, gameMap.map(x)(y)) match {
+          case (LABORATORY_BUILDING, tile:Laboratory) =>
+            lastCoordinates = CoordinateImpl(tile.topLeftCoordinate.x + tile.doorCoordinates.x, tile.topLeftCoordinate.y + tile.doorCoordinates.y + 1)
+          case (POKEMON_CENTER_BUILDING, tile: PokemonCenter) =>
             lastCoordinates = CoordinateImpl(tile.topLeftCoordinate.x + tile.doorCoordinates.x, tile.topLeftCoordinate.y + tile.doorCoordinates.y + 1)
           case _ =>
         }
       }
     }
-    new LaboratoryController(this.view, this, trainer).start()
   }
 
   override protected def doPause(): Unit = {
@@ -49,15 +57,7 @@ class MapController(private val view: View, private val _trainer: Trainer) exten
   override protected def doResume(): Unit = {
     if(trainer.getFirstAvailableFavouritePokemon <= 0) {
       DBConnect.rechangeAllTrainerPokemon(trainer.id)
-      for( x <- 0 until gameMap.width) {
-        for (y <- 0 until gameMap.height) {
-          gameMap.map(x)(y) match {
-            case tile: PokemonCenter =>
-              lastCoordinates = CoordinateImpl(tile.topLeftCoordinate.x + tile.doorCoordinates.x, tile.topLeftCoordinate.y + tile.doorCoordinates.y + 1)
-            case _ =>
-          }
-        }
-      }
+      updateLastCoordinateToBuilding(POKEMON_CENTER_BUILDING)
     }
     trainer.coordinate = lastCoordinates
     initView()
