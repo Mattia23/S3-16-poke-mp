9d14b6202fad117b43a1c0a3c0479ba639552041
Giulia Lucchi <giulia.lucchi3@studio.unibo.it>
2017-07-16 16:03:52 +0200

    Modified User.scala

--- src/main/scala/distributed/User.scala
+++ src/main/scala/distributed/User.scala
@@ -1,22 +1,21 @@
 package distributed
 
-import model.entities.TrainerSprites
-import model.environment.Coordinate
-
 trait User {
   def userId: Int
 
   def username: String
 
-  def sprites: TrainerSprites
+  def idImage: Int
 
-  def position: Coordinate
+  def xPosition: Int
+
+  def yPosition: Int
 }
 
 object User {
-  def apply(userId: Int, username: String, sprites: TrainerSprites, position: Coordinate): User =
-    new UserImpl(userId, username, sprites, position)
+  def apply(userId: Int, username: String, idImage: Int, xPosition: Int, yPosition: Int): User =
+    new UserImpl(userId, username, idImage, xPosition, yPosition)
 }
 
 class UserImpl(override val userId: Int, override val username: String,
-               override val sprites: TrainerSprites, override val position: Coordinate) extends User
+               override val idImage: Int, override val xPosition: Int, override val yPosition: Int) extends User
