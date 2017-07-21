703cd206fe2f74c17dfd04ea7209168e494304ed
Gianluca Grossi <grossigianluca22@gmail.com>
2017-07-21 16:11:18 +0200

    refactor InitialMenuPanel

--- src/main/java/view/JUtil.java
+++ src/main/java/view/JUtil.java
@@ -3,6 +3,8 @@ package view;
 import javax.swing.*;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class JUtil {
     public static void setFocus(JComponent jComponent){
@@ -20,6 +22,17 @@ public class JUtil {
             @Override
             public void ancestorMoved(AncestorEvent event) {
 
+            }
+        });
+    }
+
+    public static void setEnterClick(JButton jButton){
+        jButton.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    jButton.doClick();
+                }
             }
         });
     }
