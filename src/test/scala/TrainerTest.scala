790e7c81a21ca70a8dc50a5f83ee4c2f0e64e740
Francesco Dicara <dicaraf@gmail.com>
2017-08-01 09:26:59 +0200

    Updated trainer test

--- src/test/scala/TrainerTest.scala
+++ src/test/scala/TrainerTest.scala
@@ -5,82 +5,101 @@ import database.remote.DBConnect
 import model.entities._
 import org.scalatest.FunSuite
 
+import scala.collection.mutable
+
 class TrainerTest extends FunSuite{
 
   def fixture =
     new {
-      DBConnect.deleteUserAndRelatedData("prova")
-      val map = scala.collection.mutable.Map[String,JTextField]()
+      val map: mutable.Map[String, JTextField] = scala.collection.mutable.Map[String,JTextField]()
       val name = new JTextField("prova")
       val surname = new JTextField("prova")
       val email = new JTextField("prova@prova.it")
-      val username = new JTextField("prova")
-      val password = new JTextField("prova")
+      val username = new JTextField("test")
+      val password = new JTextField("testtest")
       map += "Name" -> name
       map += "Surname" -> surname
       map += "Email" -> email
       map += "Username" -> username
       map += "Password" -> password
       DBConnect.insertCredentials(collection.JavaConverters.mapAsJavaMap(map),3)
-      val trainer: Trainer = DBConnect.getTrainerFromDB("prova").get()
       val pokemon: PokemonWithLife = PokemonFactory.createPokemon(Owner.WILD,Optional.empty(),Optional.of(10)).get()
       val pokemonBehaviour: PokemonBehaviour = new PokemonBehaviourImpl(pokemon)
-      //val autoIncrementMet = DBConnect.getAutoIncrement("pokemon_met")
+      val autoIncrementMet: Int = DBConnect.getAutoIncrement("pokemon_met")
       val autoIncrementCaptured: Int = DBConnect.getAutoIncrement("pokemon")
     }
 
+  test("Create trainer from DB") {
+    val f = fixture
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    assert(trainer.username == "test")
+    assert(trainer.experiencePoints == 0)
+    assert(trainer.level == 1)
+    DBConnect.deleteUserAndRelatedData("test")
+  }
+
   test("New pokemon met, update pokedex") {
     val f = fixture
-    f.trainer.addMetPokemon(23)
-    assert(f.trainer.pokedex.pokedex == List(23))
-    f.trainer.addMetPokemon(24)
-    assert(f.trainer.pokedex.pokedex == List(23,24))
-    f.trainer.addMetPokemon(23)
-    assert(f.trainer.pokedex.pokedex == List(23,24))
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    trainer.addMetPokemon(23)
+    assert(trainer.pokedex.pokedex == List(23))
+    trainer.addMetPokemon(24)
+    assert(trainer.pokedex.pokedex == List(23,24))
+    trainer.addMetPokemon(23)
+    assert(trainer.pokedex.pokedex == List(23,24))
+    DBConnect.deleteUserAndRelatedData("test")
   }
 
   test("New pokemon added to favourite list") {
     val f = fixture
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(f.trainer.id).get())
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured)
-    assert(f.trainer.favouritePokemons == List(f.autoIncrementCaptured,0,0,0,0,0))
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured+1)
-    assert(f.trainer.favouritePokemons == List(f.autoIncrementCaptured,f.autoIncrementCaptured+1,0,0,0,0))
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured)
-    assert(f.trainer.favouritePokemons == List(f.autoIncrementCaptured,f.autoIncrementCaptured+1,0,0,0,0))
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(trainer.id).get())
+    trainer.addFavouritePokemon(f.autoIncrementCaptured)
+    assert(trainer.favouritePokemons == List(f.autoIncrementCaptured,0,0,0,0,0))
+    trainer.addFavouritePokemon(f.autoIncrementCaptured+1)
+    assert(trainer.favouritePokemons == List(f.autoIncrementCaptured,f.autoIncrementCaptured+1,0,0,0,0))
+    trainer.addFavouritePokemon(f.autoIncrementCaptured)
+    assert(trainer.favouritePokemons == List(f.autoIncrementCaptured,f.autoIncrementCaptured+1,0,0,0,0))
+    DBConnect.deleteUserAndRelatedData("test")
   }
 
   test("Change pokemon in favourite list") {
     val f = fixture
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(f.trainer.id).get())
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured)
-    assert(f.trainer.favouritePokemons == List(f.autoIncrementCaptured,0,0,0,0,0))
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(trainer.id).get())
+    trainer.addFavouritePokemon(f.autoIncrementCaptured)
+    assert(trainer.favouritePokemons == List(f.autoIncrementCaptured,0,0,0,0,0))
+    DBConnect.deleteUserAndRelatedData("test")
   }
 
   test("Update trainer") {
     val f = fixture
-    assert(f.trainer.experiencePoints == 0)
-    assert(f.trainer.level == 1)
-    f.trainer.updateTrainer(200)
-    assert(f.trainer.experiencePoints == 200)
-    assert(f.trainer.level == 3)
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    assert(trainer.experiencePoints == 0)
+    assert(trainer.level == 1)
+    trainer.updateTrainer(200)
+    assert(trainer.experiencePoints == 200)
+    assert(trainer.level == 3)
+    DBConnect.deleteUserAndRelatedData("test")
   }
 
   test("Get first pokemon with life in favourite pokemon list") {
     val f = fixture
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.pokemonBehaviour.insertPokemonIntoDB(f.trainer.id)
-    f.trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(f.trainer.id).get())
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured)
-    f.trainer.addFavouritePokemon(f.autoIncrementCaptured+1)
-    assert(f.trainer.getFirstAvailableFavouritePokemon == f.autoIncrementCaptured)
+    val trainer: Trainer = DBConnect.getTrainerFromDB("test").get()
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    f.pokemonBehaviour.insertPokemonIntoDB(trainer.id)
+    trainer.capturedPokemons_=(DBConnect.getCapturedPokemonList(trainer.id).get())
+    trainer.addFavouritePokemon(f.autoIncrementCaptured)
+    trainer.addFavouritePokemon(f.autoIncrementCaptured+1)
+    assert(trainer.getFirstAvailableFavouritePokemon == f.autoIncrementCaptured)
     f.pokemonBehaviour.undergoAttack(100000)
     f.pokemonBehaviour.updatePokemonTrainer(f.autoIncrementCaptured)
-    assert(f.trainer.getFirstAvailableFavouritePokemon == f.autoIncrementCaptured+1)
+    assert(trainer.getFirstAvailableFavouritePokemon == f.autoIncrementCaptured+1)
+    DBConnect.deleteUserAndRelatedData("test")
   }
 
 }
